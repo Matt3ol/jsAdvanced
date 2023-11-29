b97e609d0699f2616783a3d0452e8b05a4cd190f
Matt3ol <148490319+Matt3ol@users.noreply.github.com>
2023-11-29 12:24:32 +0100

    esercizio svolto

---
+++ esercizio-8/ottavo.js
@@ -0,0 +1,7 @@
+// Utilizza una template string per creare una stringa su più righe che includa interruzioni di riga.
+
+const templateString = `template template
+templateString templatetemplate
+template string ` 
+
+console.log(templateString)
