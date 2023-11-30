8d816c29df7f3a7bbf549127a3b8c345ef49ee1d
Matt3ol <148490319+Matt3ol@users.noreply.github.com>
2023-11-30 12:00:28 +0100

    esercizio svolto

---
+++ esercizio-11/undicesimo.js
@@ -0,0 +1,15 @@
+// Dato uno studente oggetto nidificato con informazioni sulle proprietà e voti, utilizza la destrutturazione dell'oggetto per creare variabili nome, età e voto che contengano i valori dell'oggetto nidificato.
+const student = {
+  info: { name: "Bob", age: 25 },
+  grades: { math: 95, science: 89 }
+};
+
+const {
+    info : {name , age },
+    grades : {math : grade , science }
+} = student;
+
+console.log(name);
+console.log(age);
+console.log(grade);
+console.log(science)
