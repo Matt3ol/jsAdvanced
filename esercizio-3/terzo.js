001f1c3a8d99e1a18071e094d4cfb308d33b2941
Matt3ol <148490319+Matt3ol@users.noreply.github.com>
2023-12-22 11:23:54 +0100

    svolto

--- esercizio-3/terzo.js
+++ esercizio-3/terzo.js
@@ -1,10 +1,7 @@
 // Utilizza il metodo forEach che prende un array di nomi di frutta e li stampa aggiungendo un punto esclamativo a ciascun nome di frutta.
 
-
 const fruits = ["Mela", "Banana", "Kiwi", "Fragola"];
 
-fruits.forEach(fruitArray);
-
-function fruitArray(item,index,arr){
-    console.log("!["+ index +") = " + item)
-}
+fruits.forEach(function(fruits , index ){
+    console.log( [fruits] + '!')
+})
