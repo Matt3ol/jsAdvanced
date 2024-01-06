d8a9d9a59e659e6dd614920768bbb112e9ef5a8c
Matt3ol <148490319+Matt3ol@users.noreply.github.com>
2024-01-06 12:58:23 +0100

    esercizio svolto

---
+++ dom-es-1/primo.js
@@ -0,0 +1,25 @@
+// Stampa in ordine in console:
+
+// l'elemento padre di ul.
+// il secondo elemento figlio di ul.
+// l'elemento fratello successivo del secondo li.
+// l'elemento fratello precedente del secondo li.
+// Il tutto utilizzando tutte e 4 le proprietà viste nel video.
+
+const ul = document.querySelector("ul");
+
+const ulParent = ul.parentElement;
+
+console.log("elemento padre di ul", ulParent);
+
+const ulChild = ul.children[1];
+
+console.log("secondo elemento figlio di ul", ulChild);
+
+const secondLi = ulChild.nextElementSibling;
+
+console.log("elemento fratello successivo del secondo li", secondLi);
+
+const anotherLi = ulChild.previousElementSibling;
+
+console.log('elemento fratello precedente del secondo li',anotherLi)
